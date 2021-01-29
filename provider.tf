fee0ca0109ae3c2307efc1abad465f37dbf623da
Jan Tymiński <jan.tyminski@netguru.com>
2021-01-29 17:50:05 +0100

    specify minor version
    Signed-off-by: Jan Tymiński <jan.tyminski@netguru.com>

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   alias = "main"
-  version = "=> 2.9"
+  version = "=> 2.9.0"
   # description = "AWS Region for S3 and other resources"
 }
 
 provider "aws" {
   alias = "cloudfront"
-  version = "=> 2.9"
+  version = "=> 2.9.0"
   # description = "AWS Region for Cloudfront (ACM certs only supports us-east-1)"
 }
